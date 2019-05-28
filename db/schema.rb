54dcbae75c237d5157a4ea91f4aba1a83c875e1e
Carlos Cabrera <ccm.contacto2@gmail.com>
2019-05-27 21:03:30 -0400

    Se agrego migración para título en los tweets y se agregó captura de pantalla rake-routes del proyecto

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_182519) do
+ActiveRecord::Schema.define(version: 2019_05_28_004542) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
